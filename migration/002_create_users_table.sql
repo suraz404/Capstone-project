be5d66ec85e4f5adcfd8a1bdd5a414dca7011ec7
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-09-17 20:57:34 +0545

    Add database migration scripts and PostgreSQL connection setup

---
+++ migration/002_create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABEL users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash TEXT,
+    google_id VARCHAR(255) UNIQUE,
+    role VARCHAR(20) NOT NULL DEFAULT 'USER'
+      CHECK (role IN ('USER','ADMIN')),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
